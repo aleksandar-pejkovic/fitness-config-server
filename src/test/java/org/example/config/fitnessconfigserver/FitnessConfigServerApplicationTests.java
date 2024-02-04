9a38df6ba5b4ac76874e93934c4d9499a6c9b4d4
pejko <aleksandarpejkovic@hotmail.com>
2024-02-04 20:17:52 +0100

    first commit

---
+++ src/test/java/org/example/config/fitnessconfigserver/FitnessConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.config.fitnessconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
